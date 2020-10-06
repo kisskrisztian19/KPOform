c9c8cb248af0337d7d006d3d4d0e55137b6fc6ee
krisztián kiss <kisskrisztiannnnn@hotmail.com>
2020-10-06 10:24:52 +0200

    Add project files.

---
+++ KPOform/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace KPOform
+{
+    partial class frmfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblJatekos = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSzamitogep = new System.Windows.Forms.Label();
+            this.pbJatekos = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblGyoztes = new System.Windows.Forms.Label();
+            this.btnKo = new System.Windows.Forms.Button();
+            this.btnOllo = new System.Windows.Forms.Button();
+            this.btnPapir = new System.Windows.Forms.Button();
+            this.btnKilepes = new System.Windows.Forms.Button();
+            this.btnSzabaly = new System.Windows.Forms.Button();
+            this.btnUj = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbJatekos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(45, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Játékos:";
+            // 
+            // lblJatekos
+            // 
+            this.lblJatekos.AutoSize = true;
+            this.lblJatekos.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblJatekos.Location = new System.Drawing.Point(128, 13);
+            this.lblJatekos.Name = "lblJatekos";
+            this.lblJatekos.Size = new System.Drawing.Size(58, 22);
+            this.lblJatekos.TabIndex = 1;
+            this.lblJatekos.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(340, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Számítógép:";
+            // 
+            // lblSzamitogep
+            // 
+            this.lblSzamitogep.AutoSize = true;
+            this.lblSzamitogep.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSzamitogep.Location = new System.Drawing.Point(454, 13);
+            this.lblSzamitogep.Name = "lblSzamitogep";
+            this.lblSzamitogep.Size = new System.Drawing.Size(58, 22);
+            this.lblSzamitogep.TabIndex = 3;
+            this.lblSzamitogep.Text = "label4";
+            // 
+            // pbJatekos
+            // 
+            this.pbJatekos.Location = new System.Drawing.Point(33, 54);
+            this.pbJatekos.Name = "pbJatekos";
+            this.pbJatekos.Size = new System.Drawing.Size(200, 200);
+            this.pbJatekos.TabIndex = 4;
+            this.pbJatekos.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(333, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblGyoztes
+            // 
+            this.lblGyoztes.AutoSize = true;
+            this.lblGyoztes.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblGyoztes.Location = new System.Drawing.Point(254, 286);
+            this.lblGyoztes.Name = "lblGyoztes";
+            this.lblGyoztes.Size = new System.Drawing.Size(58, 22);
+            this.lblGyoztes.TabIndex = 6;
+            this.lblGyoztes.Text = "label2";
+            // 
+            // btnKo
+            // 
+            this.btnKo.Location = new System.Drawing.Point(79, 9);
+            this.btnKo.Name = "btnKo";
+            this.btnKo.Size = new System.Drawing.Size(81, 30);
+            this.btnKo.TabIndex = 7;
+            this.btnKo.Text = "Kő";
+            this.btnKo.UseVisualStyleBackColor = true;
+            // 
+            // btnOllo
+            // 
+            this.btnOllo.Location = new System.Drawing.Point(409, 9);
+            this.btnOllo.Name = "btnOllo";
+            this.btnOllo.Size = new System.Drawing.Size(81, 30);
+            this.btnOllo.TabIndex = 8;
+            this.btnOllo.Text = "Olló";
+            this.btnOllo.UseVisualStyleBackColor = true;
+            // 
+            // btnPapir
+            // 
+            this.btnPapir.Location = new System.Drawing.Point(244, 9);
+            this.btnPapir.Name = "btnPapir";
+            this.btnPapir.Size = new System.Drawing.Size(81, 30);
+            this.btnPapir.TabIndex = 9;
+            this.btnPapir.Text = "Papír";
+            this.btnPapir.UseVisualStyleBackColor = true;
+            // 
+            // btnKilepes
+            // 
+            this.btnKilepes.Location = new System.Drawing.Point(7, 413);
+            this.btnKilepes.Name = "btnKilepes";
+            this.btnKilepes.Size = new System.Drawing.Size(156, 31);
+            this.btnKilepes.TabIndex = 10;
+            this.btnKilepes.Text = "Kilépés";
+            this.btnKilepes.UseVisualStyleBackColor = true;
+            // 
+            // btnSzabaly
+            // 
+            this.btnSzabaly.Location = new System.Drawing.Point(214, 413);
+            this.btnSzabaly.Name = "btnSzabaly";
+            this.btnSzabaly.Size = new System.Drawing.Size(156, 31);
+            this.btnSzabaly.TabIndex = 11;
+            this.btnSzabaly.Text = "Szabályok";
+            this.btnSzabaly.UseVisualStyleBackColor = true;
+            // 
+            // btnUj
+            // 
+            this.btnUj.Location = new System.Drawing.Point(421, 413);
+            this.btnUj.Name = "btnUj";
+            this.btnUj.Size = new System.Drawing.Size(156, 31);
+            this.btnUj.TabIndex = 12;
+            this.btnUj.Text = "Új játék";
+            this.btnUj.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.lblGyoztes);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.pbJatekos);
+            this.panel1.Controls.Add(this.lblSzamitogep);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lblJatekos);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(9, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(567, 330);
+            this.panel1.TabIndex = 13;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Silver;
+            this.panel2.Controls.Add(this.btnPapir);
+            this.panel2.Controls.Add(this.btnOllo);
+            this.panel2.Controls.Add(this.btnKo);
+            this.panel2.Location = new System.Drawing.Point(8, 341);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(567, 48);
+            this.panel2.TabIndex = 14;
+            // 
+            // frmfo
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.ButtonDropDown;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnUj);
+            this.Controls.Add(this.btnSzabaly);
+            this.Controls.Add(this.btnKilepes);
+            this.Name = "frmfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kő, Papír, Olló";
+            ((System.ComponentModel.ISupportInitialize)(this.pbJatekos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblJatekos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSzamitogep;
+        private System.Windows.Forms.PictureBox pbJatekos;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblGyoztes;
+        private System.Windows.Forms.Button btnKo;
+        private System.Windows.Forms.Button btnOllo;
+        private System.Windows.Forms.Button btnPapir;
+        private System.Windows.Forms.Button btnKilepes;
+        private System.Windows.Forms.Button btnSzabaly;
+        private System.Windows.Forms.Button btnUj;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
